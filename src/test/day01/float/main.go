89bfc7461125a351aacfd25a56ba3fb538fa4856
daniel-927 <120637792@qq.com>
2019-12-18 16:06:38 +0800

    test

--- src/test/day01/float/main.go
+++ src/test/day01/float/main.go
@@ -1,12 +1,15 @@
 package main
 
 import "fmt"
-import "math"
-
 
 // 浮点数
 
-func main(){
-	// math.MaxFloa32 // float32 最大值
-
+func main() {
+	// math.MaxFloat32 // float32最大值
+	f1 := 1.23456
+	fmt.Printf("%T\n",f1) // 默认Go语言中的小树都是float64类型
+	f2 := float32(1.23456)
+	fmt.Printf("%T\n",f2) // 显示声明float32类型
+	
+	
 }
